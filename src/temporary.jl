4da69521d1fe6f1b50d09c12c70e0d54c43c24dc
Joel Dahne <joel@dahne.eu>
2021-08-21 19:46:58 +0200

    Update compat for Arblib to 0.4.1 and remove temporary method
    The temporary method is now implemented directly in Arblib.

--- src/temporary.jl
+++ src/temporary.jl
@@ -1,5 +1,2 @@
 # This file contains temporary methods which will likely be added to
 # Arblib later on
-
-# Without this the method is ambiguous
-Base.:^(x::ArbSeries, y::Rational) = x^Arb(y, prec = precision(x))
