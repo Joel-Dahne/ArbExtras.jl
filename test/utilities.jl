3550280ce4ece7fa6b73c28a38e23f28a43362e9
Joel Dahne <joel@dahne.eu>
2021-09-01 03:40:05 +0200

    Add tests for bisect_interval_recursive

--- test/utilities.jl
+++ test/utilities.jl
@@ -27,6 +27,15 @@
     )
 end
 
+@testset "bisect_interval_recursive" begin
+    a, b = Arf(0), Arf(8)
+    @test ArbExtras.bisect_interval_recursive(a, b, 0) == [(0, 8)]
+    @test ArbExtras.bisect_interval_recursive(a, b, 1) == [(0, 4), (4, 8)]
+    @test ArbExtras.bisect_interval_recursive(a, b, 2) == [(0, 2), (2, 4), (4, 6), (6, 8)]
+    @test ArbExtras.bisect_interval_recursive(a, b, 3) ==
+          [(0, 1), (1, 2), (2, 3), (3, 4), (4, 5), (5, 6), (6, 7), (7, 8)]
+end
+
 @testset "check_tolerance" begin
     @test ArbExtras.check_tolerance(Arb((1, 1.1)), atol = 0.2, rtol = 0.2)
     @test ArbExtras.check_tolerance(Arb((1, 1.1)), atol = 0.2, rtol = 0.05)
