ac9ded532caa0716bc579cd2dcc2b7258e5eac80
Joel Dahne <joel@dahne.eu>
2021-09-10 12:02:50 -0400

    Add besselj method for ArbSeries

--- src/ArbExtras.jl
+++ src/ArbExtras.jl
@@ -1,6 +1,6 @@
 module ArbExtras
 
-using Arblib
+using Arblib, SpecialFunctions
 
 include("temporary.jl")
 include("utilities.jl")
@@ -11,5 +11,7 @@ include("refine_root.jl")
 include("extrema_polynomial.jl")
 include("extrema_series.jl")
 include("extrema_enclosure.jl")
+
+include("special_functions.jl")
 
 end # module
