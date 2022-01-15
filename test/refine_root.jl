c716d4e1135f8a0cb5e0714ce6bed6c6c2a2c74a
Joel Dahne <joel@dahne.eu>
2022-01-15 09:49:24 -0500

    Add new method refine_root_bisection

--- test/refine_root.jl
+++ test/refine_root.jl
@@ -112,3 +112,127 @@
         ArbExtras.refine_root(f, Arblib.add_error!(Arb(1.1), Mag(1)), max_iterations = 20),
     )
 end
+
+@testset "refine_root_bisection" begin
+    # Define a number of problems for which we expect the method to
+    # work without issues. The problems are defined as (f, enclosure,
+    # answer, atol, rtol).
+    problems = [
+        # Problems with the root away from zero so that the relative
+        # tolerance is relevant
+        [
+            (sin, Arblib.add_error!(root, Mag(1)), root, 0, sqrt(sqrt(eps(Arb)))) for
+            root in Arb(π) .* [-10:-1; 1:10]
+        ]...,
+        (
+            x -> exp(x - 1) - 1,
+            Arblib.add_error!(Arb(1.5), Mag(1)),
+            Arb(1),
+            0,
+            sqrt(sqrt(eps(Arb))),
+        ),
+        (
+            x -> exp(x + 1) - 1,
+            Arblib.add_error!(Arb(-1.5), Mag(1)),
+            Arb(-1),
+            0,
+            sqrt(sqrt(eps(Arb))),
+        ),
+
+        # Problems where the root is at zero so that the absolute
+        # tolerance is relevant
+        (sin, Arblib.add_error!(Arb(0.1), Mag(1)), Arb(0), sqrt(sqrt(eps(Arb))), 0),
+        (
+            x -> exp(x) - 1,
+            Arblib.add_error!(Arb(0.1), Mag(1)),
+            Arb(0),
+            sqrt(sqrt(eps(Arb))),
+            0,
+        ),
+        # Zero is exactly at the midpoint
+        (sin, Arblib.add_error!(Arb(0), Mag(1)), Arb(0), sqrt(sqrt(eps(Arb))), 0),
+
+        # ArbPoly
+        (
+            Arblib.fromroots(ArbPoly, [1, 2, 3]),
+            Arblib.add_error!(Arb(2.001), Mag(0.01)),
+            Arb(2),
+            0,
+            sqrt(sqrt(eps(Arb))),
+        ),
+        (
+            Arblib.fromroots(ArbPoly, [-Arb(π), Arb(π)]),
+            Arb((3, 4)),
+            Arb(π),
+            0,
+            sqrt(sqrt(eps(Arb))),
+        ),
+    ]
+
+    # Check that it works in the good cases
+    for (f, enclosure, answer, atol, rtol) in problems
+        root =
+            Arb(ArbExtras.refine_root_bisection(f, getinterval(enclosure)...; atol, rtol))
+        @test Arblib.overlaps(root, answer)
+        @test ArbExtras.check_tolerance(root; atol, rtol)
+    end
+
+    # Test that it returns NaN in case the endpoints have the same sign unless strict = false
+    @test all(isnan, ArbExtras.refine_root_bisection(x -> x^2, Arf(-1), Arf(1)))
+    @test ArbExtras.refine_root_bisection(x -> x^2, Arf(-1), Arf(1), strict = false) ==
+          (-1, 1)
+    @test_logs (:warn, "sign of endpoints don't differ") match_mode = :any ArbExtras.refine_root_bisection(
+        x -> x^2,
+        Arf(-1),
+        Arf(1),
+        verbose = true,
+    )
+
+    # Test that it returns NaN in case the sign of the endpoints can't
+    # be determined unless strict = false
+    @test all(isnan, ArbExtras.refine_root_bisection(x -> x^2 - 1, Arf(-1), Arf(1)))
+    @test ArbExtras.refine_root_bisection(x -> x^2 - 1, Arf(-1), Arf(1), strict = false) ==
+          (-1, 1)
+    @test_logs (:warn, "could not determine sign at right endpoint") match_mode = :any ArbExtras.refine_root_bisection(
+        x -> x^2 - 1,
+        Arf(-1),
+        Arf(1),
+        verbose = true,
+    )
+
+    # Check behaviour when sign of midpoint can't be determined
+    @test_logs (:info, "could not determine sign at midpoint - shifting midpoint") match_mode =
+        :any ArbExtras.refine_root_bisection(sin, Arf(-1), Arf(1), verbose = true)
+    # Case when shifting midpoint also fails
+    @test ArbExtras.refine_root_bisection(
+        x -> ifelse(abs(x) < 1, zero(x), x),
+        Arf(-1),
+        Arf(1),
+    ) == (-1, 1)
+    @test_logs (:info, "could not determine sign at new midpoint") match_mode = :any ArbExtras.refine_root_bisection(
+        x -> ifelse(abs(x) < 1, zero(x), x),
+        Arf(-1),
+        Arf(1),
+        verbose = true,
+    )
+
+    # Check that it stops once the midpoint is equal to one of the
+    # endpoints
+    a, b = ArbExtras.refine_root_bisection(
+        sin,
+        Arf(3),
+        Arf(4),
+        rtol = 0,
+        max_iterations = 1000,
+    )
+    @test (a + b) / 2 == a
+    @test_logs (:info, "midpoint equal to endpoint, maximum precision reached") match_mode =
+        :any ArbExtras.refine_root_bisection(
+        sin,
+        Arf(3),
+        Arf(4),
+        rtol = 0,
+        max_iterations = 1000,
+        verbose = true,
+    )
+end
